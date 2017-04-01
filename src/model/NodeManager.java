6ee5bf81b7b506c18e02938e2c38e619936917d2
Danil P <slemonide@gmail.com>
2017-03-31 23:39:58 -0700

    Add ui and start backend.

---
+++ src/model/NodeManager.java
@@ -0,0 +1,22 @@
+package model;
+
+import javax.swing.text.html.HTML;
+
+/**
+ * Keeps information about the current node and its children
+ */
+public class NodeManager {
+
+    /**
+     * Singleton pattern
+     */
+    private NodeManager() {}
+
+    public HTML getCurrentPage() {
+        return null; // stub
+    }
+
+
+
+
+}
